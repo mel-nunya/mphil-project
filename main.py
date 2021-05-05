45dd9e6e7a354d09955a220a0990bfcef4556b6d
Khoby-790 <ebaidoo79@gmail.com>
2021-05-05 21:53:26 +0000

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -5,9 +5,20 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers.normalization import BatchNormalization
 from tensorflow.keras.layers.advanced_activations import LeakyReLU
+import cv2
+
 
 # CONSTANTS TO BE DEFINED HERE
 batch_size = 64
 epochs = 20
 num_classes = 10
+
+
+# Prepare an algorithm to bring in the dataset
+
+
+# Preprocess the images
+
+
+# 
 
